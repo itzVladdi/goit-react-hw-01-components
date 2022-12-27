45ea7a07cb0c0e53adc7a03fd72adb1bd8831fc7
Vladislav Marinin <czargion@gmail.com>
2022-12-28 01:35:35 +0200

    add four tasks without styles

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id} className="item">
+          <span className="status"></span>
+          <img className="avatar" src={avatar} alt="User avatar" width="48" />
+          <p className="name">{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.node.isRequired,
+    }).isRequired
+  ).isRequired,
+};
